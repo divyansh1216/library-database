419adb5a00f12027a8b39b076567555f4ab0def3
Divyansh Tailor <divyanshtailor36@gmail.com>
2023-09-13 17:08:14 +0530

    Requests successfully executed

--- library.js
+++ library.js
@@ -1,55 +1,98 @@
 const express = require('express');
-const yup = require('yup');
-const mysql = require('mysql2');
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+
 const app = express();
+const port = 5432;
+
 app.use(express.json());
 
-const librarySchema = yup.object().shape({
-    cardnumber: yup.string(),
-    firstname: yup.string().required(),
-    lastname: yup.string().required(),
-    status: yup.string(),
+
+// To get Books and Categories
+app.get('/books', async (req, res) => {
+    const books = await prisma.Book.findMany();
+    console.log('Retrieved data from the "book" table:', books);
+    res.json(books); 
+})
+
+app.get('/category', async (req, res) => {
+    const category = await prisma.Category.findMany();
+    // console.log('Retrieved data from the "category" table:', category);
+    res.json(category); 
 });
 
-const connection = mysql.createConnection({
-    host: '127.0.0.1',
-    user: 'root',
-    password: 'dipu3635',
-    database: 'library',
+// To post data in books and categories
+app.post('/books', async (req, res) => {
+    const newBook = await prisma.Book.create({
+      data: {
+        id:req.body.id,
+        title:req.body.title,
+        categoryId:req.body.categoryId,
+      },
+    });  
+    res.json(newBook)
 });
-
-connection.connect((error) => {
-    if (error) {
-        console.error('Failed to connect to the database:', error);
-        process.exit(1);
-    } else {
-        console.log('Connected to the database');
-    }
-});
-app.get('/Users', (req, res) => {
-    connection.query('SELECT * FROM Users', (error, results) => {
-        if (error) {
-            console.error(error);
-            return res.status(500).json({ error: 'Internal server error' });
-        }
-        res.json(results);
-    });
-});
-app.get('/Users/:cardnumber', (req, res) => {
-  const cardnumber = req.params.cardnumber; // Use req.params.cardnumber to access the route parameter
-  connection.query('SELECT * FROM Users WHERE cardnumber = ?', [cardnumber], (error, results) => {
-      if (error) {
-          console.error(error);
-          res.status(500).json({ error: 'Internal Server Error' });
-      } else {
-          if (results.length > 0) {
-              res.json(results[0]);
-          } else {
-              res.status(404).json({ error: 'User not found' });
-          }
-      }
+app.post('/category', async (req, res) => {
+      const { name } = req.body; 
+      // Create the category
+      const newCategory = await prisma.Category.create({
+        data: {
+            id:req.body.id,
+            name:req.body.name,
+          },
+      });  
+      res.json(newCategory);
   });
-});
-app.listen(8587);
 
+
+
+// To Update category and Books by Id
+app.patch("/books/:booksId",async (req,res)=>
+  {
+      const { booksId } = req.params;
+      const { name} = req.body;
+      const updatedbooks = await prisma.book.update({
+          where: { id: parseInt(booksId) },
+          data: {
+            title:req.body.title,
+            categoryId:req.body.categoryId,
+          }
+        })
+        res.json(updatedbooks);
+        });
+app.patch("/category/:categoryId",async (req,res)=>
+  {
+      const { categoryId } = req.params;
+      const { name} = req.body;
+      const updatedCategory = await prisma.category.update({
+          where: { id: parseInt(categoryId) },
+          data: {
+            name:req.body.name,
+          }
+        })
+        res.json(updatedCategory);
+        });
+  
+  
+// To Delete Category and Books by Id 
+app.delete("/category/:categoryId", async (req, res) => {
+    const { categoryId } = req.params;
+    const deletedCategory = await prisma.category.delete({
+      where: { id: parseInt(categoryId) },
+    });
+    res.json(deletedCategory);
+  });
+app.delete("/books/:booksId", async (req, res) => {
+    const { booksId } = req.params;
+    const deletedbooks = await prisma.book.delete({
+      where: { id: parseInt(booksId) },
+    });
+    res.json(deletedbooks);
+  });
+ 
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+ 
+ 
 
---
+++ prisma/migrations/20230913075614_divyansh/migration.sql
@@ -0,0 +1,19 @@
+-- CreateTable
+CREATE TABLE "Category" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "Category_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Book" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "categoryId" INTEGER,
+
+    CONSTRAINT "Book_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Book" ADD CONSTRAINT "Book_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "Category"("id") ON DELETE SET NULL ON UPDATE CASCADE;
